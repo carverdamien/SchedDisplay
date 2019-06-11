1c295e54360efd9dac73cf1882e7229832202fc2
Damien Carver <carverdamien@gmail.com>
2019-06-11 14:22:10 +0200

    checkData

--- intervals.py
+++ intervals.py
@@ -6,10 +6,11 @@ import h5py
 def main():
     path = './data/idle_intervals/linux.hdf5'
     def handleData(data):
+        twidth = 960
         tmin = min(data[i][0][0] for i in range(len(data)))
         tmax = max(data[i][1][-1] for i in range(len(data)))
-        slider_t0 = bk.models.widgets.Slider(
-            title="t0",
+        slider_tmin = bk.models.widgets.Slider(
+            title="tmin",
             value=0,
             start=tmin,
             end=tmax,
@@ -20,12 +21,12 @@ def main():
             for l in range(len(data))
         ]
         def update_source(attrname, old, new):
-            print('TODO')
+            print('TODO: attrname={} old={} new={}'.format(attrname, old, new))
             pass
-        for widget in [slider_t0]:
+        for widget in [slider_tmin]:
             widget.on_change('value', update_source)
         interactivity = bk.layouts.column(
-            slider_t0,
+            slider_tmin,
         )
         plot = bkplt.figure(
             plot_height=540,
@@ -51,9 +52,21 @@ def main():
     loadData(path, handleData)
     pass
 
+def checkData(data):
+    for i in range(len(data)):
+        print('Checking Data {}'.format(i))
+        inf, sup = data[i]
+        # inf < sup
+        assert(np.sum(sup < inf) == 0)
+        # inf is sorted
+        assert(np.sum(np.diff(inf) < 0) == 0)
+        # sup is sorted
+        assert(np.sum(np.diff(sup) < 0) == 0)
+        pass
+
 def loadData(path, handle):
     with h5py.File(path, 'r') as h:
         data = [np.array(h[cpu]) for cpu in sorted(list(h.keys()), key=lambda e:int(e))]
+        checkData(data)
         handle(data)
-
 main()
