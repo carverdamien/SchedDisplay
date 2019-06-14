7f36acd50b9e6d52849735d34802a30ef8f6c373
Damien Carver <carverdamien@gmail.com>
2019-06-14 15:52:42 +0200

    add color

--- sched_monitor_view/bg/updateSource.py
+++ sched_monitor_view/bg/updateSource.py
@@ -10,44 +10,47 @@ def plot(data, event_selected, interval_selected, callback):
 def target(data, event_selected, interval_selected, callback):
 	source_event_data = []
 	source_interval_data = []
-	for i in range(len(Types.EVENT)):
-		if i not in event_selected:
-			source_event_data.append(dict(x0=[], y0=[], x1=[], y1=[]))
-			continue
-		x0 = np.array([])
-		x1 = np.array([])
-		y0 = np.array([])
-		y1 = np.array([])
-		for cpu in data:
-			timestamp = np.array(data[cpu]['timestamp'])
-			event = np.array(data[cpu]['event'])
-			sel = event == i
-			N = len(timestamp[sel])
-			x0 = np.append(x0, timestamp[sel])
-			x1 = np.append(x1, timestamp[sel])
-			y0 = np.append(y0, float(cpu) * np.ones(N))
-			y1 = np.append(y1, (float(cpu)+.75) * np.ones(N))
-		source_event_data.append(dict(x0=x0, y0=y0, x1=x1, y1=y1))
-	for i in range(len(Types.INTERVAL)):
-		if i not in interval_selected or i not in HANDLER:
-			source_interval_data.append(dict(x0=[], y0=[], x1=[], y1=[]))
-			continue
-		source_interval_data.append(HANDLER[i](data))
+	y_offset = 0
+	for path in data:
+		for i in range(len(Types.EVENT)):
+			if i not in event_selected:
+				source_event_data.append(dict(x0=[], y0=[], x1=[], y1=[]))
+				continue
+			x0 = np.array([])
+			x1 = np.array([])
+			y0 = np.array([])
+			y1 = np.array([])
+			for cpu in data[path]:
+				timestamp = np.array(data[path][cpu]['timestamp'])
+				event = np.array(data[path][cpu]['event'])
+				sel = event == i
+				N = len(timestamp[sel])
+				x0 = np.append(x0, timestamp[sel])
+				x1 = np.append(x1, timestamp[sel])
+				y0 = np.append(y0, (float(cpu)+y_offset) * np.ones(N))
+				y1 = np.append(y1, (float(cpu)+.75+y_offset) * np.ones(N))
+			source_event_data.append(dict(x0=x0, y0=y0, x1=x1, y1=y1))
+		for i in range(len(Types.INTERVAL)):
+			if i not in interval_selected or i not in HANDLER:
+				source_interval_data.append(dict(x0=[], y0=[], x1=[], y1=[]))
+				continue
+			source_interval_data.append(HANDLER[i](data[path], y_offset))
+		y_offset += len(data[path])
 	callback(source_event_data, source_interval_data)
 
-def RQ_SIZE_eq_0(data):
-	return interval(data, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.eq, 0)
+def RQ_SIZE_eq_0(data, y_offset):
+	return interval(data, y_offset, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.eq, 0)
 
-def RQ_SIZE_gt_0(data):
-	return interval(data, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.gt, 0)
+def RQ_SIZE_gt_0(data, y_offset):
+	return interval(data, y_offset, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.gt, 0)
 
-def RQ_SIZE_eq_1(data):
-	return interval(data, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.eq, 1)
+def RQ_SIZE_eq_1(data, y_offset):
+	return interval(data, y_offset, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.eq, 1)
 
-def RQ_SIZE_gt_1(data):
-	return interval(data, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.gt, 1)
+def RQ_SIZE_gt_1(data, y_offset):
+	return interval(data, y_offset, Types.ID_EVENT['RQ_SIZE'], 'arg0', operator.gt, 1)
 
-def interval(data, event_id, measurement, op, value):
+def interval(data, y_offset, event_id, measurement, op, value):
 	r = {
 		k : np.array([])
 		for k in ['x0','x1','y0','y1']
@@ -61,20 +64,20 @@ def interval(data, event_id, measurement, op, value):
 		sel = event == event_id
 		timestamp = np.array(data[cpu]['timestamp'])[sel]
 		m = np.array(data[cpu][measurement])[sel]
-		_r = interval_per_cpu(cpu, timestamp, tmax, m, op, value)
+		_r = interval_per_cpu(float(cpu)+y_offset, timestamp, tmax, m, op, value)
 		for k in r:
 			r[k] = np.append(r[k], _r[k])
 	return r
 
-def interval_per_cpu(cpu, timestamp, tmax, measurement, op, value):
+def interval_per_cpu(y_offset, timestamp, tmax, measurement, op, value):
 	sel = op(measurement, value)
 	if np.sum(sel) == 0:
 		return dict(x0=[], y0=[], x1=[], y1=[])
 	x0 = timestamp[sel]
 	x1 = np.append(timestamp[1:],[tmax])[sel]
 	N = len(x0)
-	y0 = float(cpu) * np.ones(N)
-	y1 = float(cpu) * np.ones(N)
+	y0 = y_offset * np.ones(N)
+	y1 = y_offset * np.ones(N)
 	return dict(x0=x0, y0=y0, x1=x1, y1=y1)
 
 HANDLER = {
--- sched_monitor_view/main.py
+++ sched_monitor_view/main.py
@@ -1,7 +1,9 @@
 # External imports
 from bokeh.layouts import row, column
 from bokeh.plotting import curdoc, figure
-from bokeh.models.widgets import Select, CheckboxGroup, Button
+from bokeh.models.glyphs import Segment
+from bokeh.models import Legend, LegendItem
+from bokeh.models.widgets import Select, CheckboxGroup, Button, Dropdown, ColorPicker
 from bokeh.models import ColumnDataSource
 from tornado import gen
 from functools import partial
@@ -11,23 +13,42 @@ import feeds.fspath
 import bg.loadData
 import bg.updateSource
 
+TYPES = Types.EVENT+Types.INTERVAL
+data = {}
+color = {
+    k:'#FFFFFF'
+    for k in TYPES
+}
 # Build the components
 doc = curdoc()
-checkgroup_event = [
-    CheckboxGroup(labels=[e], margin=[0,0,0,0])
-    for e in Types.EVENT
-]
-checkgroup_interval = [
-    CheckboxGroup(labels=[e], margin=[0,0,0,0])
-    for e in Types.INTERVAL
-]
 select_hdf5 = Select(
-    title ='Data:'
+    title ='Path:',
+    sizing_mode="fixed",
 )
 button_load_hdf5 = Button(
     label="Load",
+    align="end",
     button_type="success",
+    width_policy="min",
+)
+select_types = Select(
+    title="Types",
     sizing_mode="fixed",
+    options=TYPES,
+    value=TYPES[0],
+)
+colorpicker_types = ColorPicker(
+    width=50,
+    sizing_mode="fixed",
+    align='end',
+    color='#FFFFFF',
+)
+button_plot = Button(
+    align='end',
+    label="Plot",
+    button_type="success",
+    width_policy="min",
+    disabled=True,
 )
 figure_plot = figure(
     sizing_mode='stretch_both',
@@ -46,33 +67,39 @@ source_interval = [
     )
     for i in range(len(Types.INTERVAL))
 ]
+items=[]
+index=0
 segment_event = [
-    figure_plot.segment(
-        'x0',
-        'y0',
-        'x1',
-        'y1',
-        source=source_event[i],
+    Segment(
+        x0='x0',
+        x1='x1',
+        y0='y0',
+        y1='y1',
+        line_color='#FFFFFF',
     )
     for i in range(len(source_event))
 ]
+
+for i in range(len(source_event)):
+    r = figure_plot.add_glyph(source_event[i], segment_event[i])
+    items.append(LegendItem(label=Types.EVENT[i], renderers=[r], index=index))
+    index+=1
 segment_interval = [
-    figure_plot.segment(
-        'x0',
-        'y0',
-        'x1',
-        'y1',
-        source=source_interval[i],
+    Segment(
+        x0='x0',
+        x1='x1',
+        y0='y0',
+        y1='y1',
+        line_color='#FFFFFF'
     )
     for i in range(len(source_interval))
 ]
-button_plot = Button(
-    label="Plot",
-    button_type="success",
-    sizing_mode="fixed",
-    disabled=True,
-)
-
+for i in range(len(source_interval)):
+    r = figure_plot.add_glyph(source_interval[i], segment_interval[i])
+    items.append(LegendItem(label=Types.INTERVAL[i], renderers=[r], index=index))
+    index+=1
+legend = Legend(items=items)
+figure_plot.add_layout(legend)
 # Add feeds
 root = './raw/hackbench/monitored'
 ext  = '.hdf5'
@@ -85,21 +112,57 @@ def callback_fspath(l):
 feeds.fspath.feed(root,ext,callback_fspath).start()
 
 # Add interactivity
-data = {}
+def on_change_select_hdf5(attr, old, new):
+    path = select_hdf5.value
+    if path in data:
+        button_load_hdf5.label = 'rm'
+        button_load_hdf5.button_type = 'warning'
+    else:
+        button_load_hdf5.label = 'add'
+        button_load_hdf5.button_type = 'success'
+    pass
+select_hdf5.on_change('value', on_change_select_hdf5)
+def on_change_select_types(attr, old, new):
+    type_selected = select_types.value
+    colorpicker_types.color = color[type_selected]
+select_types.on_change('value', on_change_select_types)
+def on_change_colorpicker_types(attr, old, new):
+    type_selected = select_types.value
+    new_color = colorpicker_types.color
+    if type_selected in Types.EVENT:
+        i = Types.ID_EVENT[type_selected]
+        segment_event[i].line_color = new_color
+    elif type_selected in Types.INTERVAL:
+        i = Types.ID_INTERVAL[type_selected]
+        segment_interval[i].line_color = new_color
+    else:
+        raise Exception()
+    color[type_selected] = new_color
+    pass
+colorpicker_types.on_change('color', on_change_colorpicker_types)
 @gen.coroutine
 def coroutine_loadData(path, new_data):
-    data.clear()
-    data.update(new_data)
+    if path in data:
+        raise Exception()
+    data[path]=new_data
+    button_load_hdf5.label = 'rm'
+    button_load_hdf5.button_type = 'warning'
     button_load_hdf5.disabled = False
     button_plot.disabled = False
-    select_hdf5.title = "Data: {}".format(path)
 def callback_loadData(path, new_data):
     doc.add_next_tick_callback(partial(coroutine_loadData, path, new_data))
 def on_click_loadhdf5(new):
-    button_load_hdf5.disabled = True
-    button_plot.disabled = True
     path = select_hdf5.value
-    bg.loadData.load(path, callback_loadData).start()
+    if path in data:
+        del data[path]
+        button_load_hdf5.label = 'add'
+        button_load_hdf5.button_type = 'success'
+        if len(data) == 0:
+             button_plot.disabled = True
+    else:
+        button_load_hdf5.disabled = True
+        button_plot.disabled = True
+        bg.loadData.load(path, callback_loadData).start()
 button_load_hdf5.on_click(on_click_loadhdf5)
 @gen.coroutine
 def coroutine_plot(source_event_data, source_interval_data):
@@ -121,24 +184,21 @@ def callback_plot(source_event_data, source_interval_data):
 def on_click_plot(new):
     button_load_hdf5.disabled = True
     button_plot.disabled = True
-    event = [i for i in range(len(Types.EVENT)) if checkgroup_event[i].active]
-    interval = [i for i in range(len(Types.INTERVAL)) if checkgroup_interval[i].active]
+    event    = [i for i in range(len(Types.EVENT))    if color[Types.EVENT[i]]    != '#FFFFFF']
+    interval = [i for i in range(len(Types.INTERVAL)) if color[Types.INTERVAL[i]] != '#FFFFFF']
     bg.updateSource.plot(data, event, interval, callback_plot).start()
 button_plot.on_click(on_click_plot)
 # assamble components
 root = column(
-    figure_plot,
     row(
         select_hdf5,
         button_load_hdf5,
+        select_types,
+        colorpicker_types,
+        button_plot,
         sizing_mode = 'scale_width',
     ),
-    row(
-        column(*checkgroup_event),
-        column(*checkgroup_interval),
-        sizing_mode = 'scale_width',
-    ),
-    button_plot,
+    figure_plot,
     sizing_mode = 'stretch_both',
 )
 doc.add_root(root)
