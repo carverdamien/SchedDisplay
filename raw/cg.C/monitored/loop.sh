1a0f0b2bb78395487fc20083bbefd23f453c8843
Damien Carver <carverdamien@gmail.com>
2019-06-18 16:37:43 +0200

    cg.C/monitored

--- raw/cg.C/monitored/loop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-for TASKS in 40 1000 2000 4000 6000 8000 10000
-do
-    TIMEOUT=60 PATH_TO_IPANEMA_MODULE= TASKS=${TASKS} ./entrypoint
-    mv ouput.hdf5 ${TASKS}-linux.hdf5
-done
