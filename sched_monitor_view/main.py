b52f6cd6bdfa1ca948de2ee331f9b25d6961d7f6
Damien Carver <carverdamien@gmail.com>
2019-06-21 14:00:16 +0200

    refactor. make field hdf5 coherent between USER_VIEW JSON_VIEW

--- sched_monitor_view/main.py
+++ sched_monitor_view/main.py
@@ -15,9 +15,23 @@ import state
 ################ Build the components ################
 ######################################################
 doc = curdoc()
+USER_VIEW = 0
+JSON_VIEW = 1
+DATA_VIEW = 2
+PLOT_VIEW = 3
+TABS        = [   USER_VIEW,   JSON_VIEW,   DATA_VIEW,   PLOT_VIEW, ]
+labels_TABS = [ "User View", "JSON View", "Data View", "Plot View", ]
+OBJECTS = {
+	k:[]
+	for k in TABS
+}
+UPDATES = {
+	k:[]
+	for k in TABS
+}
 ################ TABS View ################
 radiobuttongroup_tab = RadioButtonGroup(
-	labels=["User View", "JSON View", "Data View", "Plot View"],
+	labels=labels_TABS,
 )
 ################ User View ################
 select_hdf5 = Select(
@@ -31,7 +45,7 @@ button_add_or_rm_hdf5 = Button(
     width_policy="min",
     visible=False,
 )
-USER_VIEW = [select_hdf5,button_add_or_rm_hdf5]
+OBJECTS[USER_VIEW].extend([select_hdf5,button_add_or_rm_hdf5])
 ################ JSON View ################
 textareainput_json = TextAreaInput(visible=False)
 button_import_json = Button(
@@ -41,12 +55,12 @@ button_import_json = Button(
     width_policy="min",
     visible=False,
 )
-JSON_VIEW = [textareainput_json, button_import_json]
+OBJECTS[JSON_VIEW].extend([textareainput_json, button_import_json])
 ################ Data View ################
 source_datatable = ColumnDataSource({'empty_data':[]})
 columns_datatable = [ TableColumn(field="empty_data", title="empty data") ]
 datatable = DataTable(source=source_datatable, columns=columns_datatable, visible=False)
-DATA_VIEW = [datatable]
+OBJECTS[DATA_VIEW].extend([datatable])
 ################ Plot View ################
 figure_plot = figure(
     sizing_mode='stretch_both',
@@ -55,14 +69,7 @@ figure_plot = figure(
     output_backend="webgl",
     visible=False,
 )
-PLOT_VIEW = [figure_plot]
-################ All Views ################
-VIEWS = [
-	USER_VIEW,
-	JSON_VIEW,
-	DATA_VIEW,
-	PLOT_VIEW,
-]
+OBJECTS[PLOT_VIEW].extend([figure_plot])
 ###########################################
 ################ Add feeds ################
 ###########################################
@@ -79,16 +86,18 @@ feeds.fspath.feed('./raw', '.hdf5',callback_fspath).start()
 ################ TABS View ################
 def on_click_radiobuttongroup_tab(new):
 	selected = radiobuttongroup_tab.active
-	for i in range(len(VIEWS)):
-		for e in VIEWS[i]:
-			if i == selected:
-				e.visible = True
+	for f in UPDATES[selected]:
+		f()
+	for view in TABS:
+		for o in OBJECTS[view]:
+			if view == selected:
+				o.visible = True
 			else:
-				e.visible = False
+				o.visible = False
 	pass
 radiobuttongroup_tab.on_click(on_click_radiobuttongroup_tab)
 ################ User View ################
-def label_button_add_or_rm_hdf5():
+def update_button_add_or_rm_hdf5():
 	path = select_hdf5.value
 	if state.hdf5_is_loaded(path):
 		button_add_or_rm_hdf5.label = 'rm'
@@ -97,7 +106,7 @@ def label_button_add_or_rm_hdf5():
 		button_add_or_rm_hdf5.label = 'add'
 		button_add_or_rm_hdf5.button_type = 'success'
 def on_change_select_hdf5(attr, old, new):
-    label_button_add_or_rm_hdf5()
+    update_button_add_or_rm_hdf5()
 select_hdf5.on_change('value', on_change_select_hdf5)
 def on_click_loadhdf5(new):
 	path = select_hdf5.value
@@ -105,8 +114,31 @@ def on_click_loadhdf5(new):
 		state.unload_hdf5(path)
 	else:
 		state.load_hdf5(path)
-	label_button_add_or_rm_hdf5()
+	update_button_add_or_rm_hdf5()
 button_add_or_rm_hdf5.on_click(on_click_loadhdf5)
+UPDATES[USER_VIEW].extend([update_button_add_or_rm_hdf5])
+################ JSON View ################
+def on_click_button_import_json(new):
+	new_state = textareainput_json.value
+	state.from_json(new_state)
+	update_button_import_json()
+button_import_json.on_click(on_click_button_import_json)
+def update_button_import_json():
+	new_state = textareainput_json.value
+	if state.is_valid(new_state):
+		button_import_json.disabled = False
+	else:
+		button_import_json.disabled = True
+def update_textareainput_json():
+	value = state.to_json()
+	textareainput_json.value = value
+def on_change_textareainput_json(attr, old, new):
+	update_button_import_json()
+textareainput_json.on_change('value', on_change_textareainput_json)
+UPDATES[JSON_VIEW].extend([
+	update_textareainput_json,
+	update_button_import_json,
+])
 #####################################################
 ################ Assamble components ################
 #####################################################
--- sched_monitor_view/state.py
+++ sched_monitor_view/state.py
@@ -1,10 +1,23 @@
-STATE = {}
+import json
 
-TODO = []
-
+STATE = {
+	'hdf5' : [],
+}
+def from_json(new_state):
+	new_state = json.loads(new_state)
+	STATE.clear()
+	STATE.update(new_state)
+def to_json():
+	return json.dumps(STATE)
+def is_valid(new_state):
+	try:
+		return not to_json() == json.dumps(json.loads(new_state))
+	except json.decoder.JSONDecodeError as e:
+		pass
+	return False
 def hdf5_is_loaded(path):
-	return path in TODO
+	return path in STATE['hdf5']
 def load_hdf5(path):
-	TODO.append(path)
+	STATE['hdf5'].append(path)
 def unload_hdf5(path):
-	TODO.remove(path)
+	STATE['hdf5'].remove(path)
