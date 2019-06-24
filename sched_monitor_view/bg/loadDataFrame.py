e4fbe0d516bd84bd0f380fb6e1afc1e8e0f85c0a
Damien Carver <carverdamien@gmail.com>
2019-06-24 15:13:57 +0200

    extract comm too slow

--- sched_monitor_view/bg/loadDataFrame.py
+++ sched_monitor_view/bg/loadDataFrame.py
@@ -2,6 +2,9 @@ from threading import Thread
 import h5py
 import numpy as np
 import pandas as pd
+import itertools
+
+EXEC_EVT = 0
 
 def bg(*args):
 	return Thread(target=fg, args=args)
@@ -29,4 +32,17 @@ def fg(path, path_id, callback, done):
 	df['cpu'] = np.concatenate([np.array([int(cpu)]*len(data[cpu][keys[0]])) for cpu in cpus])
 	df['path_id'] = np.array([path_id]*len(df['cpu']))
 	df = pd.DataFrame(df)
+	df['comm'] = np.array(['']*len(df))
+	sel = df['event'] == EXEC_EVT
+	for pid, arg0, arg1 in itertools.zip_longest(df['pid'][sel], df['arg0'][sel], df['arg1'][sel]):
+		sel = df['pid'] == pid
+		arg0 = int(arg0)
+		arg1 = int(arg1)
+		b = (arg0<<32|arg1).to_bytes(8, byteorder="little")
+		i = 0
+		while i < 7 and b[i] > 0:
+			i+=1
+		comm = b[:i].decode()
+		print(pid, b, comm)
+		df.loc[sel, 'comm'] = comm
 	callback(path, df, done)
