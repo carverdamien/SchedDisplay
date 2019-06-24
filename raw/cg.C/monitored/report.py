663a918420868f498d4dffc83ce7fa3ad85eaa71
Damien Carver <carverdamien@gmail.com>
2019-06-24 16:31:33 +0200

    report.py computes comm

--- raw/cg.C/monitored/report.py
+++ raw/cg.C/monitored/report.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-import sys, h5py, os, mmap, struct, json
+import sys, h5py, os, mmap, struct, json, itertools
 import numpy as np
+import pandas as pd
+
+EXEC_EVT = 0
 
 def main():
     o_path, i_path = sys.argv[1:3]
@@ -24,10 +27,31 @@ def load_sched_monitor(path):
     }
 
 def load_tracer_raw(path):
-    return {
+    data = {
         cpu : load_tracer_raw_per_cpu(os.path.join(path, cpu))
         for cpu in os.listdir(path)
     }
+    cpus = list(data.keys())
+    cpus.sort(key=lambda x:int(x))
+    keys = list(data[cpus[0]].keys())
+    df = {
+	k : np.concatenate([data[cpu][k] for cpu in cpus])
+	for k in keys
+    }
+    df['cpu'] = np.concatenate([np.array([int(cpu)]*len(data[cpu][keys[0]])) for cpu in cpus])
+    df = pd.DataFrame(df)
+    df['comm'] = np.chararray(np.shape((len(df))), itemsize=8)
+    df['comm'] = ''
+    sel = df['event'] == EXEC_EVT
+    for pid, addr in itertools.zip_longest(df['pid'][sel], df['addr'][sel]):
+        sel = df['pid'] == pid
+        b = addr.to_bytes(8, byteorder="little")
+        i = 0
+        while i < 7 and b[i] > 0:
+            i+=1
+        comm = b[:i].decode()
+        df.loc[sel, 'comm'] = comm
+    return df
 
 def load_tracer_raw_per_cpu(path):
     data = {
@@ -63,15 +87,17 @@ def load_tracer_raw_per_cpu(path):
 
 def store(grp, obj):
     for key in obj:
-        if isinstance(obj[key], dict):
+        if isinstance(obj[key], (dict, pd.DataFrame)):
             new_grp = grp.create_group(key)
             store(new_grp, obj[key])
         elif isinstance(obj[key], (str,int,float)):
             grp.attrs[key] = obj[key]
         elif isinstance(obj[key], (np.ndarray,list)):
             grp.create_dataset(key,data=obj[key],compression="gzip")
+        elif isinstance(obj[key], (pd.Series)):
+            grp.create_dataset(key,data=np.array(obj[key]),compression="gzip")
         else:
-            raise Exception('Cannot store key {} of obj {}'.format(key,obj))
+            raise Exception('Cannot store key {} (type={}) of obj {}'.format(key,type(obj[key]),obj))
 
 if __name__ == '__main__':
     main()
