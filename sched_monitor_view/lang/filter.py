4bd2e94dda010a1d01b0c637eee42bf685ce35d3
Damien Carver <carverdamien@gmail.com>
2019-06-26 08:57:49 +0200

    add key_gt_value filter operator

--- sched_monitor_view/lang/filter.py
+++ sched_monitor_view/lang/filter.py
@@ -13,6 +13,8 @@ def key_equals_value(df, key, val):
 	return df[key] == val
 def key_not_equals_value(df, key, val):
 	return df[key] != val
+def key_gt_value(df, key, val):
+	return df[key] > val
 def a_and_b(df, a, b):
 	return a & b
 def a_or_b(df, a, b):
@@ -20,6 +22,7 @@ def a_or_b(df, a, b):
 OPERATOR = {
 	'==' : key_equals_value,
 	'!=' : key_not_equals_value,
+	'>'  : key_gt_value,
 	'&'  : a_and_b,
 	'|'  : a_or_b,
 }
