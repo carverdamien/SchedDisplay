701160c48b33d3a0b82a60484b3b9cfd124f76e5
Damien Carver <carverdamien@gmail.com>
2019-07-29 14:07:06 +0200

    add MONITOR option. add script to test all.

---
+++ raw/kbuild_sched/monitored/test_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TASKS=256
+TIMEOUT=36000
+
+for NO_TURBO in '0' '1'
+do
+    for SCALING_GOVERNOR in 'performance' 'powersave'
+    do
+	for PATH_TO_IPANEMA_MODULE in ''
+	do
+	    for MONITOR in '' 'y'
+	    do
+		echo ${NO_TURBO} | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+		echo ${SCALING_GOVERNOR} | sudo tee /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
+		sudo MONITOR=${MONITOR} TIMEOUT=${TIMEOUT} PATH_TO_IPANEMA_MODULE=${PATH_TO_IPANEMA_MODULE} TASKS=${TASKS} ./entrypoint
+	    done
+	done
+    done
+done
