09ebd4fee83897813b9ac6a4b96cb118b3458fa1
Damien Carver <carverdamien@gmail.com>
2019-07-30 11:27:09 +0200

    human friendly naming

--- raw/kbuild_sched/monitored/test_all.sh
+++ raw/kbuild_sched/monitored/test_all.sh
@@ -9,7 +9,7 @@ do
     do
 	for PATH_TO_IPANEMA_MODULE in '' "/lib/modules/$(uname -r)/source/ipanema/modules/cfs_wwc_local_new_unblock.ko"
 	do
-	    for MONITOR in '' 'y'
+	    for MONITOR in '' 'monitored'
 	    do
 		echo ${NO_TURBO} | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 		echo ${SCALING_GOVERNOR} | sudo tee /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
@@ -19,6 +19,14 @@ do
 		else
 		    SCHED=$(basename ${PATH_TO_IPANEMA_MODULE} .ko)
 		fi
+		case ${NO_TURBO} in
+		    '0')
+			NO_TURBO='turbo'
+			;;
+		    '1')
+			NO_TURBO=''
+			;;
+		esac
 		CSTATE=$(sed -n 's/.*\(cstate=0\).*/\1/p' /proc/cmdline)
 		HDF5=${TASKS}-${SCHED}-${SCALING_GOVERNOR}-${NO_TURBO}-${CSTATE}-${MONITOR}.hdf5
 		sudo HDF5=${HDF5} MONITOR=${MONITOR} TIMEOUT=${TIMEOUT} PATH_TO_IPANEMA_MODULE=${PATH_TO_IPANEMA_MODULE} TASKS=${TASKS} ./entrypoint
