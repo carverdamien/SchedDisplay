1d4cb2768320836f6ff69e078ac465d71db577ce
Damien Carver <carverdamien@gmail.com>
2019-08-02 12:23:55 +0200

    click hide/show

--- smv/TabViewController.py
+++ smv/TabViewController.py
@@ -8,9 +8,19 @@ class TabViewController(object):
 		self.labels = labels
 		self.controllers = controllers
 		self.views = [c.view for c in controllers]
+		for v in self.views:
+			v.visible = False
 		self.radiobuttongroup_tab = RadioButtonGroup(labels=labels)
+		self.radiobuttongroup_tab.on_click(self.on_click_radiobuttongroup_tab)
 		self.view = column(
 			row(self.radiobuttongroup_tab, sizing_mode='scale_width',),
 			row(*self.views, sizing_mode = 'stretch_both')
 		)
 		self.doc = doc
+
+	def on_click_radiobuttongroup_tab(self, new):
+		for c in self.controllers:
+			c.hide()
+		selected = self.controllers[self.radiobuttongroup_tab.active]
+		selected.show()
+		pass
--- smv/apps/app1/__init__.py
+++ smv/apps/app1/__init__.py
@@ -1,9 +1,30 @@
 from smv.TabViewController import TabViewController
 from smv.ConsoleViewController import ConsoleViewController
+from threading import Thread
 
 def modify_doc(doc):
 	labels = ['Console 0', 'Console 1']
 	console = [ConsoleViewController(doc=doc), ConsoleViewController(doc=doc)]
 	tab = TabViewController(labels, console, doc=doc)
 	doc.add_root(tab.view)
+	def target():
+		from time import sleep
+		i=0
+		while True:
+			console[0].write(i)
+			sleep(1)
+			i+=2
+	t=Thread(target=target)
+	t.daemon = True
+	t.start()
+	def target():
+		from time import sleep
+		i=1
+		while True:
+			console[1].write(i)
+			sleep(1)
+			i+=2
+	t=Thread(target=target)
+	t.daemon = True
+	t.start()
 	pass
