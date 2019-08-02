9b962cbdbeca24de21f8fd7e9fbdc69736d31954
Damien Carver <carverdamien@gmail.com>
2019-08-02 16:36:52 +0200

    finish SelectFileViewController

--- smv/SelectFileViewController.py
+++ smv/SelectFileViewController.py
@@ -1,10 +1,18 @@
 from smv.ViewController import ViewController
 from bokeh.models.widgets import Select
+import os
+
+def find_files(directory, ext):
+	for root, dirs, files in os.walk(directory, topdown=False):
+		for name in files:
+			path = os.path.join(root, name)
+			if ext == os.path.splitext(name)[1]:
+				yield path
 
 class SelectFileViewController(ViewController):
 	"""docstring for SelectFileViewController"""
-	def __init__(self, doc=None):
-		options=["foo", "bar", "baz", "quux"] # TODO
+	def __init__(self, directory, ext, doc=None):
+		options=sorted(list(find_files(directory,ext)))
 		view = Select(
 			title="Select File:",
 			value=options[0],
--- smv/apps/test_SelectFileView/__init__.py
+++ smv/apps/test_SelectFileView/__init__.py
@@ -1,5 +1,5 @@
 from smv.SelectFileViewController import SelectFileViewController
 
 def modify_doc(doc):
-	ctrl = SelectFileViewController(doc=doc)
+	ctrl = SelectFileViewController('.','.py',doc=doc)
 	doc.add_root(ctrl.view)
