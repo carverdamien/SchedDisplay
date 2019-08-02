e74d9da7452cdc46f6b7796e3826528b05a26de9
Damien Carver <carverdamien@gmail.com>
2019-08-02 18:17:00 +0200

    upload button

--- smv/LoadFileViewController.py
+++ smv/LoadFileViewController.py
@@ -1,7 +1,8 @@
 from smv.ViewController import ViewController
+from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.models.widgets import Select, Button
 from bokeh.layouts import row
-import os
+import os, io, base64
 from threading import Thread
 
 def find_files(directory, ext):
@@ -10,6 +11,37 @@ def find_files(directory, ext):
 			path = os.path.join(root, name)
 			if ext == os.path.splitext(name)[1]:
 				yield path
+
+# Special thanks to Kevin Anderson
+CUSTOM_JS_CODE = """
+function read_file(filename) {
+    var reader = new FileReader();
+    reader.onload = load_handler;
+    reader.onerror = error_handler;
+    // readAsDataURL represents the file's data as a base64 encoded string
+    reader.readAsDataURL(filename);
+}
+function load_handler(event) {
+    var b64string = event.target.result;
+    file_source.data = {'file_contents' : [b64string], 'file_name':[input.files[0].name]};
+    file_source.trigger("change");
+}
+function error_handler(evt) {
+    if(evt.target.error.name == "NotReadableError") {
+        alert("Can't read file!");
+    }
+}
+var input = document.createElement('input');
+input.setAttribute('type', 'file');
+input.onchange = function(){
+    if (window.FileReader) {
+        read_file(input.files[0]);
+    } else {
+        alert('FileReader is not supported in this browser');
+    }
+}
+input.click();
+"""
 
 class LoadFileViewController(ViewController):
 	"""docstring for LoadFileViewController"""
@@ -22,20 +54,33 @@ class LoadFileViewController(ViewController):
 			height=40,
 			height_policy="fixed",
 		)
-		button = Button(
+		select_button = Button(
 			label='Select',
 			align="end",
 			button_type="success",
 			width_policy="min",
 			height=40,
 			height_policy="fixed",
+		)
+		upload_button = Button(
+			label='Upload',
+			align="end",
+			button_type="success",
+			width_policy="min",
+			height=40,
+			height_policy="fixed",
 	    )
-		view = row(select, button, sizing_mode = 'scale_width',)
+		# view = row(select, select_button, upload_button, sizing_mode = 'scale_width',)
+		view = row(select, select_button, upload_button, sizing_mode = 'stretch_both',)
 		super(LoadFileViewController, self).__init__(view, doc)
 		self.select = select
-		self.button = button
+		self.select_button = select_button
+		self.upload_button = upload_button
+		self.datasource = ColumnDataSource({'file_contents':[], 'file_name':[]})
 		self.callback = None
-		self.button.on_click(self.select_on_click)
+		self.select_button.on_click(self.select_on_click)
+		self.upload_button.callback = CustomJS(args=dict(file_source=self.datasource), code=CUSTOM_JS_CODE)
+		self.datasource.on_change('data', self.file_callback)
 
 	def on_loaded(self, callback):
 		self.callback = callback
@@ -46,5 +91,16 @@ class LoadFileViewController(ViewController):
 		path = self.select.value
 		def target():
 			with open(path,'r') as f:
-				self.callback(f.read())
+				self.callback(f)
 		Thread(target=target).start()
+
+	def file_callback(self,attr,old,new):
+		if self.callback is None:
+			return
+		filename = self.datasource.data['file_name'][0]
+		raw_contents = self.datasource.data['file_contents'][0]
+		# remove the prefix that JS adds
+		prefix, b64_contents = raw_contents.split(",", 1)
+		file_contents = base64.b64decode(b64_contents)
+		file_io = io.BytesIO(file_contents)
+		self.callback(file_io)
--- smv/apps/v0/__init__.py
+++ smv/apps/v0/__init__.py
@@ -11,8 +11,8 @@ def modify_doc(doc):
 	seq = SeqViewController([load, figure], doc=doc)
 	labels = ['Main','Console']
 	tab = TabViewController(labels, [seq, console], doc=doc)
-	def on_loaded(data):
-		console.write('{} bytes loaded'.format(len(data)))
+	def on_loaded(io):
+		console.write('{} bytes loaded'.format(len(io.read())))
 		seq.next()
 	load.on_loaded(on_loaded)
 	doc.add_root(tab.view)
