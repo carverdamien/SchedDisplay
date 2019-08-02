fe87294f73ba24314a898acdb0a44ba3141e4ac6
Damien Carver <carverdamien@gmail.com>
2019-08-02 11:08:10 +0200

    mv app0 test_ConsoleView

--- smv/__init__.py
+++ smv/__init__.py
@@ -1,13 +1,13 @@
 import logging
 from bokeh.server.server import Server
-from smv.apps.app0 import modify_doc as app0
+from smv.apps.test_ConsoleView import modify_doc as test_ConsoleView
 from smv.apps.app1 import modify_doc as app1
 
 def main():
 	FORMAT = '%(asctime)-15s %(message)s'
 	logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 	server = Server({
-		'/app0' : app0,
+		'/test_ConsoleView' : test_ConsoleView,
 		'/app1' : app1,
 	})
 	server.start()
