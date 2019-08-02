cae2e885c53585d142f9c83016e0a07435c9cd6b
Damien Carver <carverdamien@gmail.com>
2019-08-02 12:39:17 +0200

    mv smv/apps/{app1,test_TabView}

--- smv/__init__.py
+++ smv/__init__.py
@@ -1,14 +1,14 @@
 import logging
 from bokeh.server.server import Server
 from smv.apps.test_ConsoleView import modify_doc as test_ConsoleView
-from smv.apps.app1 import modify_doc as app1
+from smv.apps.test_TabView import modify_doc as test_TabView
 
 def main():
 	FORMAT = '%(asctime)-15s %(message)s'
 	logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 	server = Server({
 		'/test_ConsoleView' : test_ConsoleView,
-		'/app1' : app1,
+		'/test_TabView' : test_TabView,
 	})
 	server.start()
 	server.io_loop.start()
