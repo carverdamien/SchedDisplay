2ea83e5218455914f8cca4417d9ec3077b8c7a48
Damien Carver <carverdamien@gmail.com>
2019-08-08 14:58:19 +0200

    labels

--- sched_monitor_view/export.py
+++ sched_monitor_view/export.py
@@ -14,7 +14,7 @@ def export(img_path, json_dict):
 	def callback_load_hdf5(path, data, done):
 		df = data['df']
 		compute_columns(df, json_dict['columns'])
-		update_plot(df, json_dict['renderers'])
+		plot(img_path, df, json_dict['renderers'])
 		# print(df)
 	def done():
 		pass
@@ -28,9 +28,9 @@ def compute_columns(df, columns):
 		)
 	pass
 
-def update_plot(df, renderers):
+def plot(img, df, renderers):
 	# df = df.iloc[np.arange(100000)]
-	print(df)
+	# print(df)
 	fig, ax = plt.subplots()
 	# lines = [[(0, 1), (1, 1)], [(2, 3), (3, 3)], [(1, 2), (1, 3)]]
 	# c     = np.array([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
@@ -40,23 +40,27 @@ def update_plot(df, renderers):
 	ymin = 0 - 1
 	ymax = 159 + 1
 	for r in renderers:
-		X0 = df[r['x0']]
-		X1 = df[r['x1']]
-		Y0 = df[r['y0']]
-		Y1 = df[r['y1']]
+		sel = sched_monitor_view.lang.filter.sel(df, r['filter'])
+		X0 = df[r['x0']][sel]
+		X1 = df[r['x1']][sel]
+		Y0 = df[r['y0']][sel]
+		Y1 = df[r['y1']][sel]
 		N = len(X0)
 		print('transpose starts')
 		L = np.transpose(np.array([[X0,X1],[Y0,Y1]]))
 		print('transpose ends')
 		print('LineCollection starts')
-		lc = LineCollection(L)
+		lc = LineCollection(L,color=r['line_color'],label=r['label'])
 		print('LineCollection ends')
 		print('add_collection starts')
 		ax.add_collection(lc)
 		print('add_collection ends')
 	ax.set_xlim(xmin, xmax)
 	ax.set_ylim(ymin, ymax)
+	ax.legend()
+	ax.set_xlabel('Time in ns')
+	ax.set_ylabel('CPU')
 	print('savefig starts')
-	fig.savefig('foo.jpg')
+	fig.savefig(img)
 	print('savefig ends')
 	pass
