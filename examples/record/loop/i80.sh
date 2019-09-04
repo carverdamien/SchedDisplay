1e12c96e26b0ca666fbff18d9846c78bac926646
Redha Gouicem <redha.gouicem@gmail.com>
2019-09-04 14:56:22 +0200

    updated scripts to use ipanema modules correctly

--- examples/record/loop/i80.sh
+++ examples/record/loop/i80.sh
@@ -2,7 +2,7 @@
 
 set -x -e -u
 
-kexec_reboot() {
+function kexec_reboot {
     if [[ ${KERNEL} != $(uname -r) ]]
     then
 	VMLINUZ="/boot/vmlinuz-${KERNEL}"
@@ -24,17 +24,28 @@ kexec_reboot() {
     fi
 }
 
+function run_bench {
+    echo ${NO_TURBO} | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+    echo ${SCALING_GOVERNOR} | sudo tee /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
+    TAR="${OUTPUT}.tar"
+    if ! [[ -e "${TAR}" ]]
+    then
+	kexec_reboot
+	./entrypoint
+    fi
+}
+
 export BENCH=bench/oltp-mysql
 export PATH_TO_IPANEMA_MODULE=''
 export TIMEOUT=36000
 export MONITORING_SCHEDULED=y
 export MONITORING_START_DELAY=60
 export MONITORING_STOP_DELAY=10
-export MONITORING TASKS OUTPUT KERNEL
+export MONITORING TASKS OUTPUT KERNEL IPANEMA_MODULE
 NO_TURBO=0
 SCALING_GOVERNOR='powersave' # or performance
 
-DEFAULT_KERNEL=4.19.0-linux-4.19-ipanema-gd6437e6b2bdd
+DEFAULT_KERNEL=4.19.0-linux-4.19-ipanema-g2bd98bf652cb
 KERNELS="${DEFAULT_KERNEL} 4.19.0-patch-local-g131fda29324a 4.19.0-patch-local-light-g9ee5320702ba 4.19.0-patch-sched-freq-g710892956166"
 MONITORINGS="monitoring/all monitoring/cpu-energy-meter monitoring/nop"
 
@@ -47,15 +58,8 @@ do
     do
 	for TASKS in 80 160 320
 	do
-	    echo ${NO_TURBO} | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
-	    echo ${SCALING_GOVERNOR} | sudo tee /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
 	    OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/${TASKS}-${KERNEL}"
-	    TAR="${OUTPUT}.tar"
-	    if ! [[ -e "${TAR}" ]]
-	    then
-		kexec_reboot
-		./entrypoint
-	    fi
+	    run_bench
 	done
     done
 done
@@ -63,28 +67,17 @@ done
 # exit 0
 
 # IPANEMA
-build_ipanema_module() {
-    (cd $(dirname ${PATH_TO_IPANEMA_MODULE}); make -B KERNEL=../..)
-}
 for KERNEL in ${DEFAULT_KERNEL}
 do
-    for PATH_TO_IPANEMA_MODULE in "/lib/modules/$(uname -r)/source/ipanema/modules/cfs_wwc.ko" \
-				      "/lib/modules/$(uname -r)/source/ipanema/modules/ule_wwc.ko"
+    PATH_TO_IPANEMA_MODULES="/lib/modules/$(uname -r)/kernel/kernel/sched/ipanema"
+    for IPANEMA_MODULE in cfs_wwc ule_wwc
     do
 	for MONITORING in ${MONITORINGS}
 	do
 	    for TASKS in 80 160 320
 	    do
-		echo ${NO_TURBO} | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
-		echo ${SCALING_GOVERNOR} | sudo tee /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
-		OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/IPANEMA=$(basename ${PATH_TO_IPANEMA_MODULE} .ko)/${TASKS}-${KERNEL}"
-		TAR="${OUTPUT}.tar"
-		if ! [[ -e "${TAR}" ]]
-		then
-		    kexec_reboot
-		    build_ipanema_module
-		    ./entrypoint
-		fi
+		OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/IPANEMA=$(basename ${MODULE})/${TASKS}-${KERNEL}"
+		run_bench
 	    done
 	done
     done
@@ -104,15 +97,8 @@ do
     do
 	for TASKS in 32
 	do
-	    echo ${NO_TURBO} | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
-	    echo ${SCALING_GOVERNOR} | sudo tee /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
 	    OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/${TASKS}-${KERNEL}"
-	    TAR="${OUTPUT}.tar"
-	    if ! [[ -e "${TAR}" ]]
-	    then
-		kexec_reboot
-		./entrypoint
-	    fi
+	    run_bench
 	done
     done
 done
