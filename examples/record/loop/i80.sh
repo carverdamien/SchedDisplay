54ea72ed444abbdbf339dd7a523ec6ab6020dff9
Jean-Pierre Lozi <jean-pierre@lozi.org>
2019-09-10 20:31:26 +0200

    Current directory fix in 'prepare' script

--- examples/record/loop/i80.sh
+++ examples/record/loop/i80.sh
@@ -50,59 +50,59 @@ DEFAULT_KERNEL=4.19.0-linux-4.19-ipanema-g2bd98bf652cb
 KERNELS="${DEFAULT_KERNEL} 4.19.0-patch-local-g131fda29324a 4.19.0-patch-local-light-g9ee5320702ba 4.19.0-patch-sched-freq-g710892956166"
 MONITORINGS="monitoring/all monitoring/cpu-energy-meter monitoring/nop"
 
-##################################################
-## Running mysql OLTP
-##################################################
-#for KERNEL in ${KERNELS}
-#do
-#    for MONITORING in ${MONITORINGS}
-#    do
-#	for TASKS in 80 160 320
-#	do
-#	    OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/${TASKS}-${KERNEL}"
-#	    run_bench
-#	done
-#    done
-#done
-#
-## exit 0
-#
-## IPANEMA
-#for KERNEL in ${DEFAULT_KERNEL}
-#do
-#    PATH_TO_IPANEMA_MODULES="/lib/modules/$(uname -r)/kernel/kernel/sched/ipanema"
-#    for IPANEMA_MODULE in cfs_wwc ule_wwc
-#    do
-#	for MONITORING in ${MONITORINGS}
-#	do
-#	    for TASKS in 80 160 320
-#	    do
-#		OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/IPANEMA=$(basename ${IPANEMA_MODULE})/${TASKS}-${KERNEL}"
-#		run_bench
-#	    done
-#	done
-#    done
-#done
-#
-#
-##################################################
-## Running kbuild sched
-##################################################
-#BENCH=bench/kbuild
-#export TARGET=kernel/sched/
-#MONITORING_SCHEDULED=n
-#IPANEMA_MODULE=
-#for KERNEL in ${KERNELS}
-#do
-#    for MONITORING in ${MONITORINGS}
-#    do
-#	for TASKS in 32
-#	do
-#	    OUTPUT="output/BENCH=$(basename ${BENCH})-$(basename ${TARGET})/MONITORING=$(basename ${MONITORING})/${TASKS}-${KERNEL}"
-#	    run_bench
-#	done
-#    done
-#done
+#################################################
+# Running mysql OLTP
+#################################################
+for KERNEL in ${KERNELS}
+do
+    for MONITORING in ${MONITORINGS}
+    do
+	for TASKS in 80 160 320
+	do
+	    OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/${TASKS}-${KERNEL}"
+	    run_bench
+	done
+    done
+done
+
+# exit 0
+
+# IPANEMA
+for KERNEL in ${DEFAULT_KERNEL}
+do
+    PATH_TO_IPANEMA_MODULES="/lib/modules/$(uname -r)/kernel/kernel/sched/ipanema"
+    for IPANEMA_MODULE in cfs_wwc ule_wwc
+    do
+	for MONITORING in ${MONITORINGS}
+	do
+	    for TASKS in 80 160 320
+	    do
+		OUTPUT="output/BENCH=$(basename ${BENCH})/MONITORING=$(basename ${MONITORING})/IPANEMA=$(basename ${IPANEMA_MODULE})/${TASKS}-${KERNEL}"
+		run_bench
+	    done
+	done
+    done
+done
+
+
+#################################################
+# Running kbuild sched
+#################################################
+BENCH=bench/kbuild
+export TARGET=kernel/sched/
+MONITORING_SCHEDULED=n
+IPANEMA_MODULE=
+for KERNEL in ${KERNELS}
+do
+    for MONITORING in ${MONITORINGS}
+    do
+	for TASKS in 32
+	do
+	    OUTPUT="output/BENCH=$(basename ${BENCH})-$(basename ${TARGET})/MONITORING=$(basename ${MONITORING})/${TASKS}-${KERNEL}"
+	    run_bench
+	done
+    done
+done
 
 
 #################################################
