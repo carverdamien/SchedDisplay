0fb97906d6347dbb37e8e0038cc29dae3789b391
Damien Carver <carverdamien@gmail.com>
2019-09-16 11:27:09 +0200

    parse

--- smv/ConfigFile.py
+++ smv/ConfigFile.py
@@ -1,3 +1,21 @@
 import json
+from string import Template
+
 def loads(o):
-	return json.loads(o)
+	d = json.loads(o)
+	d = parse(d)
+	return d
+
+def parse(o, **kwargs):
+	if isinstance(o, dict):
+		for k in o:
+			o[k] = parse(o[k], **kwargs)
+	elif isinstance(o, list):
+		for i in range(len(o)):
+			o[i] = parse(o[i], **kwargs)
+	elif isinstance(o, str):
+		t = Template(o)
+		o = t.substitute(**kwargs)
+	else:
+		pass
+	return o
