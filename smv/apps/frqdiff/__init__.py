fdfe4a0ab94527e10d814332923462a8313557ca
Damien Carver <carverdamien@gmail.com>
2019-09-27 19:43:47 +0200

    frqdiff index

--- smv/apps/frqdiff/__init__.py
+++ smv/apps/frqdiff/__init__.py
@@ -8,6 +8,7 @@ import re, os
 
 def modify_doc(doc):
 	PATTERN = '.*BENCH=phoronix/POWER=.*/MONITORING=.*/PHORONIX=.*/.*/.*.tar'
+	PATTERN = '.*BENCH=phoronix/POWER=.*/MONITORING=.*/PHORONIX=redis/.*/.*.tar' # debug
 	def find_files(directory, ext, regexp=".*"):
 		regexp = re.compile(regexp)
 		for root, dirs, files in os.walk(directory, topdown=False):
@@ -15,67 +16,34 @@ def modify_doc(doc):
 				path = os.path.join(root, name)
 				if ext == os.path.splitext(name)[1] and regexp.match(path):
 					yield path
+	def PHORONIXES(fname):
+		return np.unique([re.search(f'(?<=PHORONIX=)[^/]+', f).group(0) for f in fname])
 	def index():
-		return sorted(list(find_files('./examples/trace', '.tar', PATTERN)))
+		_index_ = []
+		fname = sorted(list(find_files('./examples/trace', '.tar', PATTERN)))
+		kernel = "4.19.0-linux-4.19-ipanema-g2bd98bf652cb"
+		for p in PHORONIXES(fname):
+			powersave = re.compile(f'.*BENCH=phoronix/POWER=powersave-y/MONITORING=cpu-energy-meter/PHORONIX={p}/{kernel}/1.tar')
+			performance = re.compile(f'.*BENCH=phoronix/POWER=performance-n/MONITORING=cpu-energy-meter/PHORONIX={p}/{kernel}/1.tar')
+			fpowersave = None
+			fperformance = None
+			for f in fname:
+				print(f)
+				if fpowersave is None and powersave.match(f):
+					fpowersave = f
+				elif fperformance is None and performance.match(f):
+					fperformance = f
+				if fperformance is not None and fpowersave is not None:
+					_index_.append((fpowersave, fperformance))
+					break
+		return _index_
 	model = TableModel(index=index)
 	BASE = [
-		[STRING, 'fname', lambda index, row: index]
+		[STRING, '_index_', lambda index, row: str(index)]
 	]
-	def var_from_fname(varname):
-		def f(index, row):
-			return re.search(f'(?<={varname}=)[^/]+', index).group(0)
-		return [STRING, varname, f]
-	BASE += [var_from_fname(varname) for varname in ['POWER', 'PHORONIX', 'MONITORING']]
 	for args in BASE:
 		kwargs = {'dtype':args[0],'name':args[1],'function':args[2]}
 		model.add_column(Column(**kwargs))
-	PERF = [
-		[FLOAT, 'usr_bin_time',  'time.err', '{pattern:F}'],
-		[FLOAT, 'sysbench_trps', 'run.out',  '{:s}transactions:{:s}{:d}{:s}({pattern:F} per sec.)'],
-	]
-	PACKAGE_ENERGY = [
-		[FLOAT, 'cpu%d_package_joules'%(i), 'cpu-energy-meter.out',  'cpu%d_package_joules={pattern:F}'%(i)]
-		for i in range(4)
-	]
-	DRAM_ENERGY    = [
-		[FLOAT, 'cpu%d_dram_joules'%(i),    'cpu-energy-meter.out',  'cpu%d_dram_joules={pattern:F}'%(i)   ]
-		for i in range(4)
-	]
-	COLUMNS = PERF + PACKAGE_ENERGY + DRAM_ENERGY
-	for args in COLUMNS:
-		model.add_column(parsable_column(*args))
-		pass
-	MAX_PHORONIX = 2
-	PHORONIX_TEST = [
-		[STRING, f'phoro_test{i}', 'phoronix.json', ['results',i,'test']]
-		for i in range(MAX_PHORONIX)
-	]
-	PHORONIX_ARGS = [
-		[STRING, f'phoro_args{i}', 'phoronix.json', ['results',i,'arguments']]
-		for i in range(MAX_PHORONIX)
-	]
-	PHORONIX_UNITS = [
-		[STRING, f'phoro_units{i}', 'phoronix.json', ['results',i,'units']]
-		for i in range(MAX_PHORONIX)
-	]
-	PHORONIX_VALUE = [
-		[FLOAT, f'phoro_value{i}', 'phoronix.json', ['results',i,'results','schedrecord','value']]
-		for i in range(MAX_PHORONIX)
-	]
-	PHORONIX = [i for j in itertools.zip_longest(PHORONIX_TEST,PHORONIX_ARGS,PHORONIX_UNITS,PHORONIX_VALUE) for i in j]
-	JSONS = PHORONIX + [[STRING, 'kernel', 'report.main.json', ['kernel','version']]]
-	for args in JSONS:
-		model.add_column(json_column(*args))
-		pass
-	TOTAL_ENERGY = [
-		[FLOAT, 'total_package_joules',lambda inedx, row: np.sum([row['cpu%d_package_joules'%(i)] for i in range(4)])],
-		[FLOAT, 'total_dram_joules',   lambda inedx, row: np.sum([row['cpu%d_dram_joules'%(i)] for i in range(4)])],
-		[FLOAT, 'total_joules',        lambda inedx, row: np.sum([row[c] for c in ['total_package_joules', 'total_dram_joules']])],
-	]
-	for args in TOTAL_ENERGY:
-		kwargs = {'dtype':args[0],'name':args[1],'function':args[2]}
-		model.add_column(Column(**kwargs))
-		pass
 	# end of model customization
 	traces = TableViewController(model=model, doc=doc)
 	scatter = ScatterViewController(model=model, doc=doc)
