05f72e0e7e3da40ce2be255027067240ee9e5ee7
Damien Carver <carverdamien@gmail.com>
2019-09-30 16:55:33 +0200

    new frqdiff

--- smv/apps/frqdiff/__init__.py
+++ smv/apps/frqdiff/__init__.py
@@ -60,7 +60,7 @@ def modify_doc(doc):
 					yield path
 	def PHORONIXES(fname):
 		return np.unique([re.search(f'(?<=PHORONIX=)[^/]+', f).group(0) for f in fname])
-	def index():
+	def fcouple():
 		_index_ = []
 		fname = sorted(list(find_files('./examples/trace', '.tar', PATTERN)))
 		kernel = "4.19.0-linux-4.19-ipanema-g2bd98bf652cb"
@@ -78,50 +78,95 @@ def modify_doc(doc):
 					_index_.append((fpowersave, fperformance))
 					break
 		return _index_
+	def get_tau(path):
+		tau = []
+		try:
+			with tarfile.open(path, 'r:') as tar:
+				for tarinfo in tar:
+					if os.path.basename(tarinfo.name) != 'phoronix.json':
+						continue
+					with tar.extractfile(tarinfo.name) as f:
+							value = json.load(f)
+							for results in value['results']:
+								tau.append((results['test'], results['arguments'], results['units']))
+		except Exception as e:
+			print(traceback.format_exc())
+		return tau
+	def index():
+		_index_ = []
+		for fpowersave, fperformance in fcouple():
+			tau_powersave   = get_tau(fpowersave)
+			tau_performance = get_tau(fperformance)
+			for tau in tau_powersave:
+				if tau in tau_performance:
+					_index_.append((fpowersave, fperformance, *tau))
+		print(_index_)
+		return _index_
 	model = TableModel(index=index)
+	BASE = [
+		[STRING, 'test', lambda index, row: index[2]],
+		[STRING, 'arguments', lambda index, row: index[3]],
+		[STRING, 'units', lambda index, row: index[4]],
+	]
+	for args in BASE:
+		kwargs = {'dtype':args[0],'name':args[1],'function':args[2]}
+		model.add_column(Column(**kwargs))
 	WHAT = ['powersave', 'performance']
 	USR_BIN_TIME = [
 		[FLOAT, 'usr_bin_time_%s'%(WHAT[j]), j, 'time.err', '{pattern:F}']
 		for j in range(2)
 	]
-	PACKAGE_ENERGY = [
-		[FLOAT, 'cpu%d_package_joules_%s'%(i,WHAT[j]), j, 'cpu-energy-meter.out',  'cpu%d_package_joules={pattern:F}'%(i)]
-		for i in range(4)
-		for j in range(2)
-	]
-	DRAM_ENERGY    = [
-		[FLOAT, 'cpu%d_dram_joules_%s'%(i,WHAT[j]), j,    'cpu-energy-meter.out',  'cpu%d_dram_joules={pattern:F}'%(i)   ]
-		for i in range(4)
-		for j in range(2)
-	]
-	COLUMNS = USR_BIN_TIME # + PACKAGE_ENERGY + DRAM_ENERGY
-	for args in COLUMNS:
-		model.add_column(parsable_column(*args))
-		pass
-	TOTAL_ENERGY = [
-		[FLOAT, 'total_package_joules_powersave',lambda inedx, row: np.sum([row['cpu%d_package_joules_powersave'%(i)] for i in range(4)])],
-		[FLOAT, 'total_package_joules_performance',lambda inedx, row: np.sum([row['cpu%d_package_joules_performance'%(i)] for i in range(4)])],
-		[FLOAT, 'total_dram_joules_powersave',lambda inedx, row: np.sum([row['cpu%d_dram_joules_powersave'%(i)] for i in range(4)])],
-		[FLOAT, 'total_dram_joules_performance',lambda inedx, row: np.sum([row['cpu%d_dram_joules_performance'%(i)] for i in range(4)])],
-		[FLOAT, 'total_joules_powersave',        lambda inedx, row: np.sum([row[c] for c in ['total_package_joules_powersave', 'total_dram_joules_powersave']])],
-		[FLOAT, 'total_joules_performance',        lambda inedx, row: np.sum([row[c] for c in ['total_package_joules_performance', 'total_dram_joules_performance']])]
-	]
-	TOTAL_ENERGY = []
-	for args in TOTAL_ENERGY:
-		kwargs = {'dtype':args[0],'name':args[1],'function':args[2]}
-		model.add_column(Column(**kwargs))
+	#PACKAGE_ENERGY = [
+	#	[FLOAT, 'cpu%d_package_joules_%s'%(i,WHAT[j]), j, 'cpu-energy-meter.out',  'cpu%d_package_joules={pattern:F}'%(i)]
+	#	for i in range(4)
+	#	for j in range(2)
+	#]
+	#DRAM_ENERGY    = [
+	#	[FLOAT, 'cpu%d_dram_joules_%s'%(i,WHAT[j]), j,    'cpu-energy-meter.out',  'cpu%d_dram_joules={pattern:F}'%(i)   ]
+	#	for i in range(4)
+	#	for j in range(2)
+	#]
+	# COLUMNS = USR_BIN_TIME # + PACKAGE_ENERGY + DRAM_ENERGY
+	# for args in COLUMNS:
+	# 	model.add_column(parsable_column(*args))
+	#	pass
+	#TOTAL_ENERGY = [
+	#	[FLOAT, 'total_package_joules_powersave',lambda inedx, row: np.sum([row['cpu%d_package_joules_powersave'%(i)] for i in range(4)])],
+	#	[FLOAT, 'total_package_joules_performance',lambda inedx, row: np.sum([row['cpu%d_package_joules_performance'%(i)] for i in range(4)])],
+	#	[FLOAT, 'total_dram_joules_powersave',lambda inedx, row: np.sum([row['cpu%d_dram_joules_powersave'%(i)] for i in range(4)])],
+	#	[FLOAT, 'total_dram_joules_performance',lambda inedx, row: np.sum([row['cpu%d_dram_joules_performance'%(i)] for i in range(4)])],
+	#	[FLOAT, 'total_joules_powersave',        lambda inedx, row: np.sum([row[c] for c in ['total_package_joules_powersave', 'total_dram_joules_powersave']])],
+	#	[FLOAT, 'total_joules_performance',        lambda inedx, row: np.sum([row[c] for c in ['total_package_joules_performance', 'total_dram_joules_performance']])]
+	#]
+	#TOTAL_ENERGY = []
+	#for args in TOTAL_ENERGY:
+	#	kwargs = {'dtype':args[0],'name':args[1],'function':args[2]}
+	#	model.add_column(Column(**kwargs))
+	def phoronix(i):
+		def function(index, row):
+			fpowersave, fperformance, test, arguments, units = index
+			if i == 0:
+				path = fpowersave
+			else:
+				path = fperformance
+			with tarfile.open(path, 'r:') as tar:
+				for tarinfo in tar:
+					if os.path.basename(tarinfo.name) != 'phoronix.json':
+						continue
+					with tar.extractfile(tarinfo.name) as f:
+							value = json.load(f)
+							for results in value['results']:
+								if results['test'] == test and results['arguments'] == arguments and results['units'] == units:
+									return results['results']['schedrecord']['value']
+			raise ColumnNotFoundException()
+		return function
 	PHORONIX_VALUE = [
-		[STRING, f'perf_test_powersave', 0, 'phoronix.json', ['results', 0, 'test']],
-		[STRING, f'perf_test_performance', 1, 'phoronix.json', ['results', 0, 'test']],
-		[STRING, f'perf_arguments_powersave', 0, 'phoronix.json', ['results', 0, 'arguments']],
-		[STRING, f'perf_arguments_performance', 1, 'phoronix.json', ['results', 0, 'arguments']],
-		[STRING, f'perf_units_powersave', 0, 'phoronix.json', ['results',0,'units']],
-		[STRING, f'perf_units_performance', 1, 'phoronix.json', ['results',0,'units']],
-		[FLOAT, f'perf_value_powersave', 0, 'phoronix.json', ['results',0,'results','schedrecord','value']],
-		[FLOAT, f'perf_value_performance', 1, 'phoronix.json', ['results',0,'results','schedrecord','value']],
+		[FLOAT, 'perf_value_powersave', phoronix(0)],
+		[FLOAT, 'perf_value_performance', phoronix(1)],
 	]
 	for args in PHORONIX_VALUE:
-		model.add_column(json_column(*args))
+		kwargs = {'dtype':args[0],'name':args[1],'function':args[2]}
+		model.add_column(Column(**kwargs))
 		pass
 	DIFF = [
 		[FLOAT, 'diff_ratio', lambda index, row: (row['perf_value_powersave'] - row['perf_value_performance'])/row['perf_value_powersave']]
